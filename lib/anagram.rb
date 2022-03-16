c000f4fef54c59801a60fc22a4efaa130688c094
victor castro <victor_castro_01@hotmail.com>
2022-03-15 22:21:10 -0400

    Anagram detector completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+    attr_reader :word, :list
+    def initialize(word)
+        @word = word
+    end
+    def match list
+        @list = list.filter do |w| 
+           w.chars.sort == @word.chars.sort
+        end
+    end
+end
+word1 = Anagram.new('Hello')
+#word1.match
+#binding.pry
